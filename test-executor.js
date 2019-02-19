2cec9504ad6957fa7f0498484da34e74f7c9270c
guseyn <guseyn@guseyns-MacBook-Pro.local>
2019-02-19 22:20:11 +0400

    run tests via batches

--- test-executor.js
+++ test-executor.js
@@ -3,5 +3,29 @@
 const { ExecutedTests } = require('test-executor')
 
 new ExecutedTests(
-  './test'
+  './test/agent'
+).after(
+  new ExecutedTests(
+    './test/http'
+  ).after(
+    new ExecutedTests(
+      './test/incoming-message'
+    ).after(
+      new ExecutedTests(
+        './test/options'
+      ).after(
+        new ExecutedTests(
+          './test/request'
+        ).after(
+          new ExecutedTests(
+            './test/response'
+          ).after(
+            new ExecutedTests(
+              './test/server'
+            )
+          )
+        )
+      )
+    )
+  )
 ).call()
