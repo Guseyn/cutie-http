cbd1430224a94444c9ad26e8f917132c5dd882e7
guseyn <guseynism@gmail.com>
2019-05-11 10:10:53 +0400

    fix vuls

--- build.js
+++ build.js
@@ -14,7 +14,7 @@ new ExecutedLint(process, './src', './test').after(
   new ExecutedTestCoverageReport(
     new ExecutedTestCoverageCheck(
       new ExecutedTestCoverage(
-        process, './test-executor.js'
+        process, './test.js'
       ),
       { lines: 99, functions: 99, branches: 100, statements: 99 }
     ), 'json-summary'
---
+++ test.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const { ExecutedTests } = require('test-executor')
+
+'use strict'
+
+const { ExecutedTests } = require('test-executor')
+const testDirs = ['./test/agent', './test/http', './test/incoming-message', './test/options', './test/request', './test/response', './test/server']
+
+const executedTests = (curIndex = 0, curExecutedTests = new ExecutedTests(testDirs[0]), tree = curExecutedTests) => {
+  if (curIndex < testDirs.length - 1) {
+    curIndex += 1
+    const nextExecutedTests = new ExecutedTests(testDirs[curIndex])
+    curExecutedTests.after(nextExecutedTests)
+    executedTests(curIndex, nextExecutedTests, tree)
+  } else {
+    tree.call()
+  }
+}
+
+executedTests()
